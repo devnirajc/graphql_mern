0410895f102176cebfbe93510df41ec84c63dff3
devnirajc <nirajd327@gmail.com>
2021-04-25 22:30:37 +0530

    line spacing removed

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -7,52 +7,52 @@ import { useForm } from '../util/hooks';
 import { FETCH_POSTS_QUERY } from '../util/graphql';
 
 function PostForm() {
-    const { values, onChange, onSubmit } = useForm(createPostCallback, {
-        body: ''
-    });
+  const { values, onChange, onSubmit } = useForm(createPostCallback, {
+    body: ''
+  });
 
-    const [createPost, { error }] = useMutation(CREATE_POST_MUTATION, {
-        variables: values,
-        update(proxy, result) {
-            const data = proxy.readQuery({
-                query: FETCH_POSTS_QUERY
-            });
-            data.getPosts = [result.data.createPost, ...data.getPosts];
-            proxy.writeQuery({ query: FETCH_POSTS_QUERY, data });
-            values.body = '';
-        }
-    });
-
-    function createPostCallback() {
-        createPost();
+  const [createPost, { error }] = useMutation(CREATE_POST_MUTATION, {
+    variables: values,
+    update(proxy, result) {
+      const data = proxy.readQuery({
+        query: FETCH_POSTS_QUERY
+      });
+      data.getPosts = [result.data.createPost, ...data.getPosts];
+      proxy.writeQuery({ query: FETCH_POSTS_QUERY, data });
+      values.body = '';
     }
+  });
 
-    return (
-        <>
-            <Form onSubmit={onSubmit}>
-                <h2>Create a post:</h2>
-                <Form.Field>
-                    <Form.Input
-                        placeholder="Hi World!"
-                        name="body"
-                        onChange={onChange}
-                        value={values.body}
-                        error={error ? true : false}
-                    />
-                    <Button type="submit" color="teal">
-                        Submit
+  function createPostCallback() {
+    createPost();
+  }
+
+  return (
+    <>
+      <Form onSubmit={onSubmit}>
+        <h2>Create a post:</h2>
+        <Form.Field>
+          <Form.Input
+            placeholder="Hi World!"
+            name="body"
+            onChange={onChange}
+            value={values.body}
+            error={error ? true : false}
+          />
+          <Button type="submit" color="teal">
+            Submit
           </Button>
-                </Form.Field>
-            </Form>
-            {error && (
-                <div className="ui error message" style={{ marginBottom: 20 }}>
-                    <ul className="list">
-                        <li>{error.graphQLErrors[0].message}</li>
-                    </ul>
-                </div>
-            )}
-        </>
-    );
+        </Form.Field>
+      </Form>
+      {error && (
+        <div className="ui error message" style={{ marginBottom: 20 }}>
+          <ul className="list">
+            <li>{error.graphQLErrors[0].message}</li>
+          </ul>
+        </div>
+      )}
+    </>
+  );
 }
 
 const CREATE_POST_MUTATION = gql`
