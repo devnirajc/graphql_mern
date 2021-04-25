5e82a4110b30e114c9e21a018d86c3597e980b83
devnirajc <nirajd327@gmail.com>
2021-04-25 16:34:50 +0530

    remove fetch query from home component and added in separate file and useContext added in same file

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useQuery } from '@apollo/react-hooks';
-import gql from 'graphql-tag';
 import { Grid, Transition } from 'semantic-ui-react';
+
+import { AuthContext } from '../context/auth';
 import PostCard from '../components/PostCard';
+import { FETCH_POSTS_QUERY } from '../util/graphql';
 
 function Home() {
-    const { loading, data: { getPosts: posts } = {} } = useQuery(FETCH_POSTS_QUERY);
+    const { user } = useContext(AuthContext);
+    const {
+        loading,
+        data: { getPosts: posts } = {}
+    } = useQuery(FETCH_POSTS_QUERY);
 
     return (
         <Grid columns={3}>
@@ -17,38 +23,17 @@ function Home() {
                     <h1>Loading posts..</h1>
                 ) : (
                     <Transition.Group>
-                        {posts && posts.map((post) => (
-                            <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
-                                <PostCard post={post} />
-                            </Grid.Column>
-                        ))}
+                        {posts &&
+                            posts.map((post) => (
+                                <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+                                    <PostCard post={post} />
+                                </Grid.Column>
+                            ))}
                     </Transition.Group>
                 )}
             </Grid.Row>
         </Grid>
-    )
+    );
 }
 
-const FETCH_POSTS_QUERY = gql`
-{
-    getPosts {
-        id
-        body
-        createdAt
-        username
-        likeCount
-        likes {
-            username
-        }
-        commentCount
-        comments {
-            id
-            username
-            createdAt
-            body
-        }
-    }
-}
-`
-
-export default Home
+export default Home;
